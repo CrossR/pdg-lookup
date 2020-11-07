2aa506372e5f8dea6c7701c21bf579d56ddecef3
CrossR <r.cross@lancaster.ac.uk>
2020-11-07 23:06:39 +0000

    Format python file.

--- util/get_reason_data.py
+++ util/get_reason_data.py
@@ -1,13 +1,14 @@
-from particle import converters, Particle
+from particle import Particle
 
 OUTPUT_FILE_NAME = "../src/data/ParticleData.re"
+
 
 def escape_latex(txt):
     return txt.replace("\\", "\\\\")
 
 
 def quoted(str):
-    return f"\"{str}\""
+    return f'"{str}"'
 
 
 def option(val, quote=False):
@@ -17,6 +18,7 @@ def option(val, quote=False):
 
         return f"Some({val})"
     return "None"
+
 
 def wrap_minus(num):
     if num < 0:
@@ -36,43 +38,49 @@ def write_particle(particle_file, p: Particle):
     charge = p.charge
     anti = "true" if p.anti_flag else "false"
 
-    particle_file.write("\n".join([
-        f"  {{",
-        f"    pdg: {wrap_minus(pdg)},",
-        f"    name: {quoted(name)},",
-        f"    mass: {option(mass)},",
-        f"    charge: {option(charge)},",
-        f"    anti: {anti},",
-        f"    latex: {quoted(escape_latex(latex))},",
-        f"    eventGenName: {option(event_gen_name, quote=True)},",
-        f"  }},",
-        ""
-    ]))
+    particle_file.write(
+        "\n".join(
+            [
+                f"  {{",
+                f"    pdg: {wrap_minus(pdg)},",
+                f"    name: {quoted(name)},",
+                f"    mass: {option(mass)},",
+                f"    charge: {option(charge)},",
+                f"    anti: {anti},",
+                f"    latex: {quoted(escape_latex(latex))},",
+                f"    eventGenName: {option(event_gen_name, quote=True)},",
+                f"  }},",
+                "",
+            ]
+        )
+    )
 
 
 def main():
     with open(OUTPUT_FILE_NAME, "w") as particle_file:
 
-        particle_file.write("\n".join([
-            "/*",
-            " * Particle data records, produced from scikit-hep Particle",
-            " * library using script in util/.",
-            " */",
-            "",
-            "open ParticleInfo;",
-            "",
-            "let data = [|",
-            ""
-        ]))
+        particle_file.write(
+            "\n".join(
+                [
+                    "/*",
+                    " * Particle data records, produced from scikit-hep Particle",
+                    " * library using script in util/.",
+                    " */",
+                    "",
+                    "open ParticleInfo;",
+                    "",
+                    "let data = [|",
+                    "",
+                ]
+            )
+        )
 
         for i, p in enumerate(Particle.all()):
             write_particle(particle_file, p)
 
-        particle_file.write("\n".join([
-            "|];",
-            "let particleMap = ParticleInfo.makeMap(data);",
-            ""
-        ]))
+        particle_file.write(
+            "\n".join(["|];", "let particleMap = ParticleInfo.makeMap(data);", ""])
+        )
 
 
 if __name__ == "__main__":
