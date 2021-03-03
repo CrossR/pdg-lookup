1485e22389b2b75ecd47c57c5f6d0853af01b245
CrossR <r.cross@lancaster.ac.uk>
2021-03-03 00:12:43 +0000

    Fix webpack config for local build.

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -44,7 +44,7 @@ module.exports = {
     path: isEnvProduction ? paths.appBuild : undefined,
     // Add /* filename */ comments to generated require()s in the output.
     pathinfo: isEnvDevelopment,
-    publicPath: '/pdg-lookup/',
+    publicPath: isEnvProduction ? '/pdg-lookup/' : '/',
   },
   optimization: {
     minimize: isEnvProduction,
